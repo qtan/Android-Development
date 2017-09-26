1dd2db18e6ccb418fd7fdf8c838c949c382193dd
Qi Tan <qtan@ualberta.ca>
2017-09-25 22:13:28 -0600

    android

---
+++ EventReporter/app/src/main/java/com/laioffer/eventreporter/Event.java
@@ -0,0 +1,30 @@
+package com.laioffer.eventreporter;
+
+/**
+ * Created by qitan on 2017/9/25.
+ */
+
+public class Event {
+    /**
+     * All data for a event.
+     */
+    private String title;
+    private String address;
+    private String description;
+
+    /**
+     * Constructor
+     */
+    public Event(String title, String address, String description) {
+        this.title = title;
+        this.address = address;
+        this.description = description;
+    }
+
+    /**
+     * Getters for private attributes of Event class.
+     */
+    public String getTitle() { return this.title; }
+    public String getAddress() { return this.address; }
+    public String getDescription() { return this.description; }
+}
---
+++ EventReporter/app/src/main/java/com/laioffer/eventreporter/MainActivity.java
@@ -0,0 +1,40 @@
+package com.laioffer.eventreporter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get ListView object from xml.
+        ListView eventListView = (ListView) findViewById(R.id.event_list);
+
+        // Initialize an adapter.
+        EventAdapter adapter = new EventAdapter(this);
+
+        // Assign adapter to ListView.
+        eventListView.setAdapter(adapter);
+    }
+
+    /**
+     * A dummy function to get fake event names.
+     *
+     * @return an array of fake event names.
+     */
+    private String[] getEventNames() {
+        String[] names = {
+                "Event1", "Event2", "Event3",
+                "Event4", "Event5", "Event6",
+                "Event7", "Event8", "Event9",
+                "Event10", "Event11", "Event12"};
+        return names;
+    }
+
+
+}
