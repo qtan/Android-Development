63af97b104db1b0e9056c989e5f7e333febdaaeb
Qi Tan <qtan@ualberta.ca>
2017-09-27 21:31:59 -0600

    fragment

--- EventReporter/app/src/main/java/com/laioffer/eventreporter/EventFragment.java
+++ EventReporter/app/src/main/java/com/laioffer/eventreporter/EventFragment.java
@@ -2,10 +2,11 @@ package com.laioffer.eventreporter;
 
 
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 
@@ -26,8 +27,24 @@ public class EventFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_event, container, false);
         ListView listView = (ListView) view.findViewById(R.id.event_list);
-        listView.setAdapter(new EventAdapter(getActivity()));
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                getEventNames());
+
+        // Assign adapter to ListView.
+        listView.setAdapter(adapter);
         return view;
+    }
+
+    private String[] getEventNames() {
+        String[] names = {
+                "Event1", "Event2", "Event3",
+                "Event4", "Event5", "Event6",
+                "Event7", "Event8", "Event9",
+                "Event10", "Event11", "Event12"};
+        return names;
+
 
     }
 
